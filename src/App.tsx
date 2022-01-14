63c50f14aeb638b7773b05f6008290a80c8710a2
Nick Graham <nick.graham@parivedasolutions.com>
2022-01-14 08:27:30 -0600

    wordle game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  FormEventHandler,
+  Reducer,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
+import { fiveLetterWords } from "./words";
+
+const SIZE = 5;
+
+function getFiveLetterWord() {
+  const index = Math.floor(fiveLetterWords.length * Math.random());
+  return fiveLetterWords[index];
+}
 
 function App() {
+  const [answer, setAnswer] = useState(getFiveLetterWord());
+  const handleRestart = () => {
+    setAnswer(getFiveLetterWord());
+  };
+
+  return <Game key={answer} answer={answer} onRestart={handleRestart} />;
+}
+
+const Game: React.FC<{
+  answer: string;
+  onRestart: () => void;
+}> = ({ answer, onRestart }) => {
+  const [guesses, addGuess] = useReducer<Reducer<string[], string>>(
+    (state, action) => [...state, action],
+    []
+  );
+  const [won, setWon] = useState(false);
+  const handleSubmitGuess = (guess: string) => {
+    addGuess(guess);
+    if (guess.toUpperCase() === answer.toUpperCase()) {
+      setWon(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {guesses.map((guess, index) => (
+        <WordResult key={index} answer={answer} guess={guess} />
+      ))}
+      {won ? (
+        <div>
+          You won!
+          <button onClick={onRestart}>Restart?</button>
+        </div>
+      ) : (
+        <Guess key={guesses.length} onSubmitGuess={handleSubmitGuess} />
+      )}
     </div>
   );
-}
+};
+
+const Guess: React.FC<{
+  onSubmitGuess: (guess: string) => void;
+}> = ({ onSubmitGuess }) => {
+  const [guess, setGuess] = useState("");
+  const handleSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
+    onSubmitGuess?.(guess);
+  };
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [inputRef]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        ref={inputRef}
+        placeholder="Guess"
+        type="text"
+        pattern="^[a-zA-Z]*$"
+        title="Only letters are allowed"
+        minLength={SIZE}
+        maxLength={SIZE}
+        value={guess}
+        onChange={(event) => setGuess(event.target.value)}
+      />
+    </form>
+  );
+};
+
+const WordResult: React.FC<{
+  guess: string;
+  answer: string;
+}> = ({ guess, answer }) => {
+  const guessC = useMemo(() => guess.toUpperCase(), [guess]);
+  const answerC = useMemo(() => answer.toUpperCase(), [answer]);
+
+  const getColor = (index: number) => {
+    if (guessC[index] === answerC[index]) {
+      return "green";
+    }
+    if (answerC.includes(guessC[index])) {
+      return "yellow";
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", maxWidth: 100 }}>
+      {guessC.split("").map((letter, index) => (
+        <div
+          style={{
+            backgroundColor: getColor(index),
+            flex: 1,
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          {letter}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
