4f033557867798116c03919df12ed73d9e56237d
Nick Graham <nick.graham@parivedasolutions.com>
2022-01-15 10:37:23 -0600

    pull out instructions page

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,14 @@ import React, {
 import { unique } from "./array";
 import { Observable, useObservable } from "./observable";
 import { isAWord, useAnswer, useNewAnswer } from "./words";
-import { Routes, Route, Link, Navigate, useParams } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useParams,
+  useNavigate,
+} from "react-router-dom";
 
 const SIZE = 5;
 
@@ -21,10 +28,12 @@ const App: React.FC = () => {
     <div className="App">
       <nav>
         <Link to="/play/new">Play New Game</Link>&nbsp;|&nbsp;
+        <Link to="/instructions">Instructions</Link>
       </nav>
       <h1>Guess the Word!</h1>
       <Routes>
         <Route path="/" element={<Navigate replace to="/play/new" />} />
+        <Route path="instructions" element={<Instructions />} />
         <Route path="play/new" element={<PlayNew />} />
         <Route path="play/:wordBankId/:answerId" element={<Play />} />
       </Routes>
@@ -38,6 +47,23 @@ const PlayNew: React.FC = () => {
     <div>Loading Game...</div>
   ) : (
     <Navigate replace to={`/play/${answer.wordBankId}/${answer.answerId}`} />
+  );
+};
+
+const Instructions: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      Guess the 5-letter word! <br />
+      Press Enter/Return to submit guess. After each guess:
+      <ul>
+        <li>Green means the letter is in the word, and in the right spot</li>
+        <li>Yellow means the letter is in the word, but not in that spot</li>
+        <li>No color means the letter is not in the word</li>
+      </ul>
+      <button onClick={() => navigate(-1)}>Back to game</button>
+    </div>
   );
 };
 
@@ -65,16 +91,6 @@ const Play: React.FC = () => {
         padding: 100,
       }}
     >
-      <div>
-        Guess the 5-letter word! <br />
-        Press Enter/Return to submit guess. After each guess:
-        <ul>
-          <li>Green means the letter is in the word, and in the right spot</li>
-          <li>Yellow means the letter is in the word, but not in that spot</li>
-          <li>No color means the letter is not in the word</li>
-        </ul>
-      </div>
-
       <div>
         <h2>
           Game ID {wordBankId}:{answerId}
