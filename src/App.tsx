b0ed8ca57490ec3bc1f27506eab52b1f73dad252
Nick Graham <nick.graham@parivedasolutions.com>
2022-01-15 12:55:15 -0600

    use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
 import { Play, PlayNew } from "./Play";
 import { Settings } from "./Settings";
 
-const rootPath = "/word-guessing";
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Guess the Word!</h1>
       <nav>
-        <Link to={`${rootPath}/play/new`}>Play New Game</Link> |&nbsp;
-        <Link to={`${rootPath}/instructions`}>Instructions</Link> |&nbsp;
-        <Link to={`${rootPath}/settings`}>Settings</Link>
+        <Link to="/play/new">Play New Game</Link> |&nbsp;
+        <Link to="/instructions">Instructions</Link> |&nbsp;
+        <Link to="/settings">Settings</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Navigate replace to="/play/new" />} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./store";
 import { Provider } from "react-redux";
 // import reportWebVitals from './reportWebVitals';
@@ -9,9 +9,9 @@ import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
