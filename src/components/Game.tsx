90a95e0c448f6ecc5c14d9647d26484beaca85fa
Nick Graham <nick.graham@parivedasolutions.com>
2022-01-15 18:08:11 -0600

    pull out victory file

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,4 @@
-import React, {
-  FormEventHandler,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import React, { FormEventHandler, useEffect, useRef, useState } from "react";
 import { useObservable } from "../utilities/observable";
 import { isAWord } from "../utilities/words";
 import { $allowNonWordGuesses } from "./Settings";
@@ -17,6 +11,7 @@ import {
 } from "../store";
 import { BACKSPACE, SUBMIT, Keyboard } from "./Keyboard";
 import { Spinner } from "./Spinner";
+import { Victory } from "./Victory";
 
 const SIZE = 5;
 
@@ -38,10 +33,6 @@ export const Game: React.FC<{
       })
     );
   };
-
-  if (!answer) {
-    return null;
-  }
 
   return (
     <div
@@ -77,60 +68,6 @@ export const Game: React.FC<{
   );
 };
 
-const Victory: React.FC<{
-  guesses: WordStatus[];
-}> = ({ guesses }) => {
-  const guessSummary = useMemo(
-    () =>
-      guesses
-        .map((guess) =>
-          guess
-            .map((letter) => {
-              if (letter.inPosition && letter.inWord) return "🟩";
-              if (letter.inWord) return "🟨";
-              return "⬛️";
-            })
-            .join("")
-        )
-        .join("\n"),
-    [guesses]
-  );
-  const shareText = useMemo(
-    () => `I won in ${guesses.length} guesses!\n` + guessSummary,
-    [guessSummary, guesses.length]
-  );
-
-  const canShare = typeof navigator.share === "function";
-  const [copied, setCopied] = useState(false);
-  const copyShareText = async () => {
-    await navigator.clipboard.writeText(
-      shareText + `\nTry for yourself at ${window.location.href}`
-    );
-    setCopied(true);
-  };
-
-  const share = () => {
-    navigator.share({
-      text: shareText,
-      title: "Word Guessing Game",
-      url: window.location.href,
-    });
-  };
-
-  return (
-    <div>
-      <p>You won!</p>
-      <pre>{guessSummary}</pre>
-      {canShare ? (
-        <button onClick={share}>Share</button>
-      ) : (
-        <button onClick={copyShareText}>Copy Share Text</button>
-      )}
-      {copied && <div>Copied!</div>}
-    </div>
-  );
-};
-
 const Guess: React.FC<{
   onSubmitGuess: (guess: string) => void;
   eliminatedLetters: string[];
@@ -158,8 +95,8 @@ const Guess: React.FC<{
       onSubmitGuess?.(guess);
     }
   };
-  const inputRef = useRef<HTMLInputElement | null>(null);
 
+  const inputRef = useRef<HTMLInputElement | null>(null);
   useEffect(() => {
     if (inputRef.current) {
       inputRef.current.focus();
@@ -228,6 +165,7 @@ const WordResult: React.FC<{
           style={{
             backgroundColor: getColor(status),
             flex: 1,
+            minWidth: 12,
             display: "flex",
             justifyContent: "center",
           }}
---
+++ src/components/Victory.tsx
@@ -0,0 +1,57 @@
+import { useMemo, useState } from "react";
+import type { WordStatus } from "../store";
+
+const canShare = typeof navigator.share === "function";
+
+export const Victory: React.FC<{
+  guesses: WordStatus[];
+}> = ({ guesses }) => {
+  const guessSummary = useMemo(
+    () =>
+      guesses
+        .map((guess) =>
+          guess
+            .map((letter) => {
+              if (letter.inPosition && letter.inWord) return "🟩";
+              if (letter.inWord) return "🟨";
+              return "⬛️";
+            })
+            .join("")
+        )
+        .join("\n"),
+    [guesses]
+  );
+  const shareText = useMemo(
+    () => `I won in ${guesses.length} guesses!\n` + guessSummary,
+    [guessSummary, guesses.length]
+  );
+
+  const [copied, setCopied] = useState(false);
+  const copyShareText = async () => {
+    await navigator.clipboard.writeText(
+      shareText + `\nTry for yourself at ${window.location.href}`
+    );
+    setCopied(true);
+  };
+
+  const share = () => {
+    navigator.share({
+      text: shareText,
+      title: "Word Guessing Game",
+      url: window.location.href,
+    });
+  };
+
+  return (
+    <div>
+      <p>You won!</p>
+      <pre>{guessSummary}</pre>
+      {canShare ? (
+        <button onClick={share}>Share</button>
+      ) : (
+        <button onClick={copyShareText}>Copy Share Text</button>
+      )}
+      {copied && <div>Copied!</div>}
+    </div>
+  );
+};
