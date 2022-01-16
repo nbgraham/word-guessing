82570a0e452e2b5bd061187ee42ed4b8d03e84b7
Nick Graham <nick.graham@parivedasolutions.com>
2022-01-15 18:55:12 -0600

    test for guess evaluation

---
+++ src/utilities/guess.test.ts
@@ -0,0 +1,36 @@
+import { evaluateGuess } from "./guess";
+
+it("reports guess status correctly", () => {
+  const result = evaluateGuess("hello", "peach");
+  expect(result).toEqual({
+    status: [
+      {
+        character: "H",
+        inWord: true,
+        inPosition: false,
+      },
+      {
+        character: "E",
+        inWord: true,
+        inPosition: true,
+      },
+      {
+        character: "L",
+        inWord: false,
+        inPosition: false,
+      },
+      {
+        character: "L",
+        inWord: false,
+        inPosition: false,
+      },
+      {
+        character: "O",
+        inWord: false,
+        inPosition: false,
+      },
+    ],
+    eliminatedLetters: ["L", "O"],
+    foundLetters: ["H", "E"],
+  });
+});
--- src/utilities/guess.ts
+++ src/utilities/guess.ts
@@ -1,17 +1,21 @@
+import { unique } from "./array";
 import type { WordStatus } from "./types";
 
 export function evaluateGuess(guess: string, answer: string) {
+  const answerC = answer.toUpperCase();
   const status: WordStatus = guess
     .toUpperCase()
     .split("")
     .map((character, i) => ({
       character,
-      inWord: answer.includes(character),
-      inPosition: answer[i] === character,
+      inWord: answerC.includes(character),
+      inPosition: answerC[i] === character,
     }));
-  const eliminatedLetters = status
-    .filter((s) => !s.inWord)
-    .map((s) => s.character);
-  const foundLetters = status.filter((s) => s.inWord).map((s) => s.character);
+  const eliminatedLetters = unique(
+    status.filter((s) => !s.inWord).map((s) => s.character)
+  );
+  const foundLetters = unique(
+    status.filter((s) => s.inWord).map((s) => s.character)
+  );
   return { eliminatedLetters, foundLetters, status };
 }
