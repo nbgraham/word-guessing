e7357861b17bc9f69c8a3acbc680c1f7752ac4e0
Nick Graham <nick.graham@parivedasolutions.com>
2022-01-22 14:36:26 -0600

    default to validating guesses are words

--- src/store/settingsSlice.ts
+++ src/store/settingsSlice.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 const settingsSlice = createSlice({
   name: "settings",
   initialState: {
-    guessesMustBeValidWords: false,
+    guessesMustBeValidWords: navigator.onLine,
     playOffline: !navigator.onLine,
   },
   reducers: {
