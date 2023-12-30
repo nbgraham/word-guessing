ab77b2bf60a2c4ad94e05c8138101a5cf40533d9
Nick Graham <nickbgraham83@gmail.com>
2023-12-29 21:28:05 -0600

    fix double letterings

--- src/utilities/guess.test.ts
+++ src/utilities/guess.test.ts
@@ -1,5 +1,144 @@
 import { chooseBestGuess, evaluateGuess, getWordMatcher } from "./guess";
 
+describe("colors double letters correctly", () => {
+  it("colors same letter twice if in answer twice", () => {
+    const result = evaluateGuess("erode", "beech");
+    expect(result).toEqual({
+      status: [
+        {
+          character: "E",
+          inWord: true,
+          inPosition: false,
+        },
+        {
+          character: "R",
+          inWord: false,
+          inPosition: false,
+        },
+        {
+          character: "O",
+          inWord: false,
+          inPosition: false,
+        },
+        {
+          character: "D",
+          inWord: false,
+          inPosition: false,
+        },
+        {
+          character: "E",
+          inWord: true,
+          inPosition: false,
+        },
+      ],
+      eliminatedLetters: ["R", "O", "D"],
+      foundLetters: ["E"],
+    });
+  });
+  it("colors same letter twice if in answer twice, correct one green", () => {
+    const result = evaluateGuess("sheen", "beech");
+    expect(result).toEqual({
+      status: [
+        {
+          character: "S",
+          inWord: false,
+          inPosition: false,
+        },
+        {
+          character: "H",
+          inWord: true,
+          inPosition: false,
+        },
+        {
+          character: "E",
+          inWord: true,
+          inPosition: true,
+        },
+        {
+          character: "E",
+          inWord: true,
+          inPosition: false,
+        },
+        {
+          character: "N",
+          inWord: false,
+          inPosition: false,
+        },
+      ],
+      eliminatedLetters: ["S", "N"],
+      foundLetters: ["H", "E"],
+    });
+  });
+  it("colors same letter only once if in answer once", () => {
+    const result = evaluateGuess("erode", "hello");
+    expect(result).toEqual({
+      status: [
+        {
+          character: "E",
+          inWord: true,
+          inPosition: false,
+        },
+        {
+          character: "R",
+          inWord: false,
+          inPosition: false,
+        },
+        {
+          character: "O",
+          inWord: true,
+          inPosition: false,
+        },
+        {
+          character: "D",
+          inWord: false,
+          inPosition: false,
+        },
+        {
+          character: "E",
+          inWord: false,
+          inPosition: false,
+        },
+      ],
+      eliminatedLetters: ["R", "D"],
+      foundLetters: ["E", "O"],
+    });
+  });
+  it("colors only second instance if it is correct", () => {
+    const result = evaluateGuess("beech", "dress");
+    expect(result).toEqual({
+      status: [
+        {
+          character: "B",
+          inWord: false,
+          inPosition: false,
+        },
+        {
+          character: "E",
+          inWord: false,
+          inPosition: false,
+        },
+        {
+          character: "E",
+          inWord: true,
+          inPosition: true,
+        },
+        {
+          character: "C",
+          inWord: false,
+          inPosition: false,
+        },
+        {
+          character: "H",
+          inWord: false,
+          inPosition: false,
+        },
+      ],
+      eliminatedLetters: ["B", "C", "H"],
+      foundLetters: ["E"],
+    });
+  });
+});
+
 it("reports guess status correctly", () => {
   const result = evaluateGuess("hello", "peach");
   expect(result).toEqual({
